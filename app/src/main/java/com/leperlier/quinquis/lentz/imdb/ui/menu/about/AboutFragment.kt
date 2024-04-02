f206199d4c19a62ce3d9e8943302143195863532
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2024-04-02 23:05:59 +0200

    menu done, about page too, still need to update content

--- app/src/main/java/com/leperlier/quinquis/lentz/imdb/ui/menu/about/AboutFragment.kt
+++ app/src/main/java/com/leperlier/quinquis/lentz/imdb/ui/menu/about/AboutFragment.kt
@@ -1,6 +1,12 @@
 package com.leperlier.quinquis.lentz.imdb.ui.menu.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,17 +21,42 @@ class AboutFragment : Fragment() {
     private lateinit var aboutViewModel: AboutViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        aboutViewModel =
-                ViewModelProviders.of(this).get(AboutViewModel::class.java)
+        aboutViewModel = ViewModelProviders.of(this).get(AboutViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val titleAbout: TextView = root.findViewById(R.id.title_about)
         val aboutText: TextView = root.findViewById(R.id.text_about)
-        aboutViewModel.text.observe(viewLifecycleOwner, Observer {
-            aboutText.text = it
-        })
+        val explanationText = root.findViewById<TextView>(R.id.text_explanation)
+
+        titleAbout.text = aboutViewModel.title
+        aboutText.text = aboutViewModel.description
+        explanationText.text = aboutViewModel.explanation
+
+        val linkedinLink1: TextView = root.findViewById(R.id.linkedin_romain)
+        val linkedinLink2: TextView = root.findViewById(R.id.linkedin_lucas)
+        val linkedinLink3: TextView = root.findViewById(R.id.linkedin_yohan)
+
+        setLink(linkedinLink1, "LinkedIn Romain", aboutViewModel.linkedInRomain)
+        setLink(linkedinLink2, "LinkedIn Lucas", aboutViewModel.linkedInLucas)
+        setLink(linkedinLink3, "LinkedIn Yohan", aboutViewModel.linkedInYohan)
+
         return root
     }
+
+    private fun setLink(textView: TextView, text: String, url: String) {
+        val spannableString = SpannableString(text)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+        }
+        spannableString.setSpan(clickableSpan, 0, text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance()
+    }
+
 }
--- app/src/main/java/com/leperlier/quinquis/lentz/imdb/ui/menu/about/AboutViewModel.kt
+++ app/src/main/java/com/leperlier/quinquis/lentz/imdb/ui/menu/about/AboutViewModel.kt
@@ -6,12 +6,13 @@ import androidx.lifecycle.ViewModel
 
 class AboutViewModel : ViewModel() {
 
-    private val aboutText = MutableLiveData<String>().apply {
-        value = "Ce projet a été réalisé par : \n" +
-                "Lentz Romain : \n" +
-                "Leperlier Lucas\n" +
-                "Quinquis Yohan  : https://www.linkedin.com/in/yohan-quinquis-a01260205/\n" +
-                "Dans le cadre du cours de développement Android des l'école des Gobelins."
-    }
-    val text: LiveData<String> = aboutText
+    val title: String = "About"
+    val description: String = "Ce projet a été réalisé par : \n" + "Lentz Romain, Leperlier Lucas et Quinquis Yohan. Dans le cadre du cours de développement Android de l'école des Gobelins."
+
+    val explanation: String = "Ce projet utilise principalement l'api de 'The Movie database' afin d'accéder aux informations. L'app se sert de Glide afin de charger les images des films, également ......."
+
+    val linkedInRomain: String = "https://www.linkedin.com/in/romain-lentz-645448200/"
+    val linkedInLucas: String = "https://www.linkedin.com/in/lucasleperlier/"
+    val linkedInYohan: String = "https://www.linkedin.com/in/yohan-quinquis-a01260205/"
+
 }
