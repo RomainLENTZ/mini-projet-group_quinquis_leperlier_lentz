4c07de1bb45964f4f434110367e9bd8619f263d8
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2024-04-02 22:38:24 +0200

    clean project and added menu with good icons

--- idbdata/src/main/java/com/leperlier/quinquis/lentz/imdb/datasources/MovieDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.leperlier.quinquis.lentz.imdb.datasources
-import com.leperlier.quinquis.lentz.imdb.data.Token
-import com.leperlier.quinquis.lentz.imdb.utils.Result
-
-interface MovieDataSource {
-    suspend fun getToken(): Result<Token>
-    suspend fun saveToken(token: Token)
-}
